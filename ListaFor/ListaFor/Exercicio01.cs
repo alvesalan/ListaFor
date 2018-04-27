11bb6642059242e41fd4d2be72eb2ac9a3b74702
alvesalan <38699636+alvesalan@users.noreply.github.com>
2018-04-27 13:48:21 -0300

    Exercicio 1  Pronto
    Exercicio pronto

---
+++ ListaFor/ListaFor/Exercicio01.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ListaFor
+{
+    class Exercicio01
+    {
+        public Exercicio01()
+        {
+            string[] Nomes = new string[25];
+
+            for(int i = 0; i <= 4; i++)
+            {
+                Console.Write("Nome: ");
+                Nomes[i] = Console.ReadLine();
+            }
+           
+           
+          Console.ReadKey();
+        }
+    }
+}
