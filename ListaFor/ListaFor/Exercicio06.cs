b391a4d1862c1d257be32455fb3ee83f7a9cdf76
alvesalan <38699636+alvesalan@users.noreply.github.com>
2018-04-28 10:40:18 -0300

    Exercicio 6 (1 Erro)
    obs:  Os contador de numero impar não conta negativos

--- ListaFor/ListaFor/Exercicio06.cs
+++ ListaFor/ListaFor/Exercicio06.cs
@@ -9,27 +9,32 @@ namespace ListaFor
     {
         public Exercicio06()
         {
+            /*Obs: Os contador de numero impar não conta negativos
+             * 
+             * 
+             * 
+            Crie um vetor que irá armazenar 10 números.Estes números deverão ser número aleatórios.  
+            Ao final apresente: ➔ Todos os números armazenados; 
+            - A somatória final dos números; 
+            - média dos números; 
+            - Quantidade de números pares; 
+            - Quantidade de números positivos; 
+            - Quantidade de números ímpares; 
+            - Quantidade de números negativos; 
+            - Quantidade de números neutros. */
+
+
+
             Random rnd = new Random();
 
             double Soma = 0, SomaPares = 0, SomaPositivos = 0, SomaImpar = 0, SomaNegativo = 0, SomaNeutro = 0;
-            double[] Numeros = new double[4];
+            double[] Numeros = new double[10];
 
             // gerador de numero aleatorio
 
-            for (int ctr = 0; ctr <= Numeros.Length; ctr++)
-            {
-                Console.WriteLine(rnd.Next(int.MinValue, int.MaxValue));
-                if (ctr % 5 == 0) ;
-
-
-            }
-
-
-
             for (int i = 0; i < Numeros.Length; i++)
-            {
-                Console.Write("Numero: ");
-                Numeros[i] = Convert.ToDouble(Console.ReadLine());
+            { 
+                Numeros[i] = Convert.ToDouble(rnd.Next(int.MinValue, int.MaxValue));
 
                 Soma = Soma + Numeros[i];
             }
@@ -38,6 +43,7 @@ namespace ListaFor
 
             Console.Clear();
 
+            // Mostrando ao usuario os numeros gerados
             for(int i = 0; i < Numeros.Length; i++)
             {
                 Console.WriteLine("Numeros armazenados: " + Numeros[i]);
@@ -45,39 +51,45 @@ namespace ListaFor
 
             for (int i = 0; i < Numeros.Length; i++)
             {
+                //Soma dos numeros pares
+
                 if(Numeros[i] % 2 == 0)
                 {
                     SomaPares = SomaPares + 1; 
                 }
 
+                //Soma positivos
                 if(Numeros[i] > 0)
                 {
                     SomaPositivos = SomaPositivos + 1;
                 }
 
+                //Soma impar
                 if (Numeros[i] % 2 == 1)
                 {
                     SomaImpar = SomaImpar + 1;
                 }
 
+                //Soma negativo
                 if (Numeros[i] < 0)
                 {
                     SomaNegativo = SomaNegativo + 1; 
                 }
 
+                // Soma neutro
                 if (Numeros[i] == 0)
                 {
                     SomaNeutro = SomaNeutro + 1; 
                 }
             }
             Console.WriteLine("");
-            Console.WriteLine("Soma dos numeros é: " + Soma);
-            Console.WriteLine("Media dos numeros é: " + Media);
-            Console.WriteLine("Quantidade de numeros pares: " + SomaPares);
-            Console.WriteLine("Quantidade dos numeros positivos: " + SomaPositivos);
-            Console.WriteLine("Quantidade de numeros impar: " + SomaImpar);
-            Console.WriteLine("Quantidade de numeros negativos: " + SomaNegativo);
-            Console.WriteLine("Quantidade de numeros neutros: " + SomaNeutro);
+            Console.WriteLine("1- Soma dos numeros é: " + Soma);
+            Console.WriteLine("2- Media dos numeros é: " + Media);
+            Console.WriteLine("3- Quantidade de numeros pares: " + SomaPares);
+            Console.WriteLine("4- Quantidade dos numeros positivos: " + SomaPositivos);
+            Console.WriteLine("5- Quantidade de numeros impar: " + SomaImpar);
+            Console.WriteLine("6- Quantidade de numeros negativos: " + SomaNegativo);
+            Console.WriteLine("7- Quantidade de numeros neutros: " + SomaNeutro);
 
 
             Console.ReadKey();
